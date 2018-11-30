4ab16f2b5c0313aebebf2f58824b3bf9e499364c
Lele Li <lilelr@163.com>
2018-11-30 19:20:18 +0800

    reset file slave.hpp

--- src/slave/slave.hpp
+++ src/slave/slave.hpp
@@ -27,7 +27,6 @@
 
 // protobuf
 #include <monitor_info.pb.h>
-#include <hardware_resource.pb.h>
 
 // chameleon headers
 #include <resource_collector.hpp>
@@ -59,11 +58,6 @@ namespace chameleon {
 
     class Slave : public ProtobufProcess<Slave> {
     public:
-<<<<<<< HEAD
-        explicit Slave():ProcessBase("slave"){}
-
-        virtual ~Slave(){}
-=======
         explicit Slave():ProcessBase("slave"){
             msp_resource_collector = make_shared<ResourceCollector>(ResourceCollector());
             msp_resource_collector->collect_hardware_resources();
@@ -72,50 +66,11 @@ namespace chameleon {
         virtual ~Slave(){
 
         }
->>>>>>> 38012e84f8897d7be4e7226e613fb97f46bbc9e0
 
-<<<<<<< HEAD
-        virtual void initialize() {
-            install<MonitorInfo>(&Slave::register_feedback, &MonitorInfo::hostname);
-            
-            /**get cpuinfomation
-             * */
-            
-        }
-
-        void get_cpuinfo_request_from_server(const string &address){
-            AllCpuInfo ac;
-            UPID clientUPID(address);
-//        Try<list<CPU>> cpus = weiguo_proc::cpus();
-
-            cpu_collector *cpus;
-            list<cpu_collector> cpu_info = cpus->get_cpu_info();
-            for(auto i= cpu_info.begin();i!=cpu_info.end();i++){
-                ac.add_sci()->set_cpuid(i->getCpuID());
-                ac.add_sci()->set_coreid(i->getCoreID());
-                ac.add_sci()->set_physicalid(i->getPhysicalID());
-                ac.add_sci()->set_cpucores(i->getCpucores());
-                ac.add_sci()->set_modelname(i->getModelname());
-                ac.add_sci()->set_cpumhz(i->getCpuMHz());
-                ac.add_sci()->set_l1dcache(i->getL1dcache());
-                ac.add_sci()->set_l1icache(i->getL1icache());
-                ac.add_sci()->set_l2cache(i->getL2cache());
-                ac.add_sci()->set_l3cache(i->getL3cache());
-            }
-            send(clientUPID,ac);
-        }
-        
-        void register_feedback(const string& hostname){
-            cout<<" receive register feedback from master"<< hostname<<endl;
-        }
-=======
         virtual void initialize();
 
         void register_feedback(const string& hostname);
->>>>>>> 5f5f6ce08eb49160d2cee4ecd9f22f73ea2469b9
 
-<<<<<<< HEAD
-=======
     private:
        shared_ptr<ResourceCollector> msp_resource_collector;
         Option<process::Owned<SlaveHeartbeater>> heartbeater;
@@ -148,7 +103,6 @@ namespace chameleon {
 
         const Duration m_interval;
 
->>>>>>> 38012e84f8897d7be4e7226e613fb97f46bbc9e0
 
     };
 }
