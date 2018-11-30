a8c944ea4c40731f18555935024d7dee7c00d617
Lele Li <lilelr@163.com>
2018-11-30 20:20:31 +0800

    modify resource_collector

--- src/slave/resource_collector.hpp
+++ src/slave/resource_collector.hpp
@@ -11,6 +11,7 @@
 #include <iostream>
 #include <memory>
 #include <vector>
+#include <string>
 
 #include <hardware_resource.pb.h>
 
@@ -20,6 +21,7 @@
 using std::cout;
 using std::endl;
 using std::vector;
+using std::string;
 using std::shared_ptr;
 using std::make_shared;
 
@@ -43,6 +45,8 @@ namespace chameleon {
 
 
             // disk collector
+            DiskCollection* disk_collection = msp_disk->get_disk_collection();
+            hr_message->set_allocated_disk_collection(disk_collection);
 //            vector<DiskInfo> d = msp_disk->get_disk_collection();
 //            for(auto it = local_diskinfos.begin();it!=local_diskinfos.end();it++){
 //                cout << "name: "                    << it->name()
@@ -60,7 +64,8 @@ namespace chameleon {
             string gpu_infos = msp_gpu->get_gpu_string();
             msp_gpu->split_gpu_string(gpu_infos);
             GPUCollection* gpu_collection = msp_gpu->get_gpu_proto();
-            hr_message
+            hr_message->set_allocated_gpu_collection(gpu_collection);
+
             int a = 4;
         }
 
