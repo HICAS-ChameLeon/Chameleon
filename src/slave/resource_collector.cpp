56207f43e6d17a00696b21b7c56bce179db8f436
Lele Li <lilelr@163.com>
2018-12-13 11:38:29 +0800

    add lemaker memory support

--- src/slave/resource_collector.cpp
+++ src/slave/resource_collector.cpp
@@ -15,8 +15,25 @@ namespace chameleon {
         hr_message->set_allocated_cpu_collection(hr_cpu_collection);
 
         // memeory collector
-        MemoryCollection* memory_collection= msp_mem->select_meminfo();
-        hr_message->set_allocated_mem_collection(memory_collection);
+            const Try<os::UTSInfo> info = os::uname();
+            CHECK_SOME(info);
+            if(info.get().machine == setting::kArmArch){
+                    MemoryCollection* memory_collection = new MemoryCollection();
+                    const string mem_size = "2048 MB";
+                    const string mem_speed="1600 MHz";
+                    const string mem_type = "DDR3";
+                    memory_collection->set_max_size(mem_size);
+                    memory_collection->set_device_quantity(1);
+                    MemInfo* mem_info = memory_collection->add_mem_infos();
+                    mem_info->set_size(mem_size);
+                    mem_info->set_speed(mem_speed);
+                    mem_info->set_type(mem_type);
+                    LOG(INFO)<<"The machine belongs to arm architecture.";
+            }else {
+                    MemoryCollection* memory_collection= msp_mem->select_meminfo();
+                    hr_message->set_allocated_mem_collection(memory_collection);
+            }
+
 
         // disk collector
         DiskCollection* disk_collection = msp_disk->get_disk_collection();
