8649b2bea94a872081c1f558ac8e091790f6d45a
Jessicallo <sherlock_vip@163.com>
2018-12-16 21:41:04 +0800

    wqn navigationbutton add

--- src/webui/js/app.js
+++ src/webui/js/app.js
@@ -11,60 +11,7 @@ app.controller('hardware', function($scope, $http) {
         url: 'http://172.20.110.228:6060/master/hardware-resources'
     }).then(function successCallback(response) {
         $scope.hardware = response.data.content;
-        //
-        //
-        // // $scope.quantities = response.data.quantity;
-        // console.log(response.data.quantity);
-        //
-        //
-        // var my_master_json = $scope.hardware;
-        // var my_master = {};
-        // my_master.hostname = "rrr";
-        // my_master.pid = my_master_json.slave_id;
-        // //my_master.port = my_master_json['port'];
-        // my_master.id = 1;
-        // my_master.label = my_master.pid;
-        // my_master.shape = 'image';
-        // my_master.image = DIR + 'Network-Pipe-icon.png';
-        // my_master.title = '主节点';
-        //
-        // my_slaves[0] = my_master;
-        // var index_slave = 1;
-        // var index_edge = 0;
-        // var my_edges = [];
-        // // if($scope.quantities>=2){
-        // //     my_edges = [$scope.quantities-1];
-        // // }
-        // // var temp_slave = {};
-        // // temp_slave.hostname = slave['hostname'];
-        // // temp_slave.pid = slave['pid'];
-        // // temp_slave.id = 23;
-        // // temp_slave.label = 12344;
-        // // temp_slave.shape ='';
-        // var nodes = new vis.DataSet(my_slaves);
-        //
-        // // console.log(my_slaves[0]);
-        // // $scope.myname = $scope.agents;
-        // // alert("www"+$scope.agents);
-        //
-        // // create an array with edges
-        // var edges = new vis.DataSet(my_edges);
-        //
-        // var container = document.getElementById('mynetwork');
-        // var data = {
-        //     nodes: nodes,
-        //     edges: edges
-        // };
-        //
-        // var options = {
-        //     interaction:{hover:true},
-        //     manipulation: {
-        //         enabled: true
-        //     }
-        // };
-        //
-        // network = new vis.Network(container, data, options);
-
+        $scope.quantities = response.data.quantity;
     }, function errorCallback(response) {
         // 请求失败执行代码
     });
@@ -76,57 +23,55 @@ runtimeApp.controller('runtimeCtrl', function($scope, $http) {
         method: 'GET',
         url: 'http://172.20.110.228:6060/master/runtime-resources'
     }).then(function successCallback(response) {
-        console.log(79);
         $scope.runtime = response.data.content;
         console.log(response.data.content);
         $scope.quantities = response.data.quantity;
         console.log(response.data.quantity);
 
         var DIR = '../icon/refresh-cl/';
-        console.log($scope.quantities);
-        var my_slaves = new Array(1);
-        console.log("21");
+        var my_slaves = new Array($scope.quantities);
 
-        // for (var i=0,len=$scope.runtime.length; i<len; i++)
-        // {
-        //     console.log(response.data.content[i]);
-        // }
-        for(var i in $scope.runtime){
-            console.log(i);
-            console.log($scope.runtime[i]);
-        }
-
-        var my_master_json = $scope.hardware;
         var my_master = {};
-        my_master.hostname = "rrr";
-        my_master.pid = 556666;
-        //my_master.port = my_master_json['port'];
+        my_slaves[0] = my_master;
         my_master.id = 1;
-        my_master.label = my_master.pid;
+        my_master.label = "master";
         my_master.shape = 'image';
         my_master.image = DIR + 'Network-Pipe-icon.png';
         my_master.title = '主节点';
 
-        my_slaves[0] = my_master;
+        //my_slaves[0] = my_master;
         var index_slave = 1;
         var index_edge = 0;
         var my_edges = [];
-        // if($scope.quantities>=2){
-        //     my_edges = [$scope.quantities-1];
-        // }
-        // var temp_slave = {};
-        // temp_slave.hostname = slave['hostname'];
-        // temp_slave.pid = slave['pid'];
-        // temp_slave.id = 23;
-        // temp_slave.label = 12344;
-        // temp_slave.shape ='';
+        if($scope.quantities>=2){
+            my_edges = [$scope.quantities-1];
+            for(var i in $scope.runtime){
+                var slave = $scope.runtime[i];
+
+                var temp_slave = {};
+                temp_slave.label = "slave"+index_slave+"@"+slave.slave_id;
+                temp_slave.id = index_slave+1;
+                temp_slave.shape ='image';
+                temp_slave.image = DIR + 'Hardware-My-Computer-3-icon.png';
+                temp_slave.title = JSON.stringify(slave);
+                my_slaves[index_slave]= temp_slave;
+                index_slave++;
+
+                var temp_edge = {};
+                temp_edge.from = 1;
+                temp_edge.to = temp_slave.id;
+                temp_edge.arrows = 'to';
+                my_edges[index_edge] = temp_edge;
+                index_edge++;
+
+            }
+        }else{
+            my_edges=[];
+        }
+
+
         var nodes = new vis.DataSet(my_slaves);
 
-        // console.log(my_slaves[0]);
-        // $scope.myname = $scope.agents;
-        // alert("www"+$scope.agents);
-
-        // create an array with edges
         var edges = new vis.DataSet(my_edges);
 
         var container = document.getElementById('mynetwork');
@@ -136,29 +81,46 @@ runtimeApp.controller('runtimeCtrl', function($scope, $http) {
         };
 
         var options = {
-            interaction:{hover:true},
-            manipulation: {
-                enabled: true
+            interaction: {
+                navigationButtons: true,
+                keyboard: true
+            },
+            groups: {
+                'switch': {
+                    shape: 'triangle',
+                    color: '#FF9900' // orange
+                },
+                desktop: {
+                    shape: 'dot',
+                    color: "#2B7CE9" // blue
+                },
+                mobile: {
+                    shape: 'dot',
+                    color: "#5A1E5C" // purple
+                },
+                server: {
+                    shape: 'square',
+                    color: "#C5000B" // red
+                },
+                internet: {
+                    shape: 'square',
+                    color: "#109618" // green
+                }
             }
+
         };
 
         network = new vis.Network(container, data, options);
 
+        // add event listeners
+        network.on('select', function(params) {
+            document.getElementById('selection').innerHTML = 'Selection: ' + params.nodes;
+        });
 
     }, function errorCallback(response) {
         // 请求失败执行代码
     });
 });
-
-// var network = angular.module('network',[]);
-// network.controller('NetworkCtrl',function ($scope) {
-//     console.log(quantities);
-//     var my_slaves = new Array(quantities);
-//
-//
-//
-//
-// })
 
 //手动加载ng-app
 angular.bootstrap(document.querySelector('[ng-app="runtimeApp"]'), ['runtimeApp']);
