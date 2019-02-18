8e93a4020f2ae734d456ad13f876942d08b6cf82
Unknown <764165887@qq.com>
2019-02-18 17:23:11 +0800

    modified

--- src/docker/docker_test.cpp
+++ src/docker/docker_test.cpp
@@ -17,15 +17,32 @@
 
 #include "docker/docker.hpp"
 #include "mesos/resources.hpp"
+#include "linux/cgroups.hpp"
+#include "linux/cgroups.cpp"
 
 using namespace process;
 
 using std::list;
 using std::string;
 using std::vector;
+using std::cout;
+using std::endl;
+
 
 int main(){
-    Try<Owned<Docker>> docker = Docker::create("docker","/var/run/docker.sock",false);
-    
+    //Resources resources = Resources::parse("cpus:1;mem:512").get();
+    Owned<Docker> docker = Docker::create("docker","/var/run/docker.sock",false).get();
+    //LOG(INFO)<< "Heldon docker path : "<< docker.get()->getPath();
+    std::cout << docker.get()->getPath() << std::endl;
+    Future<list<Docker::Container>> containers = docker->ps(true, "mesos-docker"); // ps -a
+    for(list<Docker::Container>::const_iterator i = containers.get().begin();i != containers.get().end(); ++i ){
+        cout<< "Heldon containers id : "<< i->id << endl;
+        cout<< "Heldon containers name : "<< i->name << endl;
+    }
+    //AWAIT_READY(containers);
+    foreach (const Docker::Container& container, containers.get()) {
+        cout<< "Heldon containers id : "<< container.id << endl;
+        cout<< "Heldon containers name : "<< container.name << endl;
+    }
 }
 
