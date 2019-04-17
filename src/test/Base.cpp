a5062d109d0974737fcaaee8ddbba86bd51d87f6
Lele Li <lilelr@163.com>
2019-04-17 21:14:20 +0800

    riscv apr

--- src/test/Base.cpp
+++ src/test/Base.cpp
@@ -28,64 +28,6 @@ public:
 
 
 
-//class Base2 {
-//
-//public:
-//
-//
-//
-//    virtual void f() { cout << "Base2::f" << endl; }
-//
-//
-//
-//    virtual void g() { cout << "Base2::g" << endl; }
-//
-//
-//
-//    virtual void h() { cout << "Base2::h" << endl; }
-//
-//};
-//
-//
-//
-//class Base3 {
-//
-//public:
-//
-//
-//
-//    virtual void f() { cout << "Base3::f" << endl; }
-//
-//
-//
-//    virtual void g() { cout << "Base3::g" << endl; }
-//
-//
-//
-//    virtual void h() { cout << "Base3::h" << endl; }
-//
-//};
-//
-
-
-//
-//class Derive : public Base1, public Base2, public Base3 {
-//
-//public:
-//
-//
-//
-//    virtual void f() { cout << "Derive::f" << endl; }
-//
-//
-//
-//    virtual void g1() { cout << "Derive::g1" << endl; }
-//
-//};
-
-
-
-
 typedef void(*Fun)(void);
 
 
